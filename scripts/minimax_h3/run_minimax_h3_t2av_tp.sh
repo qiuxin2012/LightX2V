907650d6b324583e30c1f088e78700cf259b0840
qiuxin2012 <qiuxin2012cs@gmail.com>
2026-08-06 07:15:42 +0000

    update

--- scripts/minimax_h3/run_minimax_h3_t2av_tp.sh
+++ scripts/minimax_h3/run_minimax_h3_t2av_tp.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-lightx2v_path=/data/nvme6/gushiqiao/codes/LightX2V
-model_path=/data/nvme6/gushiqiao/models/MiniMax-H3
+lightx2v_path=/llm/xin/LightX2V-main
+model_path=/llm/models/MiniMax-H3
 
-export CUDA_VISIBLE_DEVICES=0,1
+export PLATFORM=intel_xpu
 
 source ${lightx2v_path}/scripts/base/base.sh
 export DTYPE=BF16
 export SENSITIVE_LAYER_DTYPE=FP32
 
-torchrun --standalone --nproc_per_node=2 -m lightx2v.infer \
+torchrun --standalone --nproc_per_node=4 -m lightx2v.infer \
 --model_cls minimax_h3 \
 --task t2av \
 --model_path ${model_path} \
 --config_json ${lightx2v_path}/configs/minimax_h3/minimax_h3_t2av_tp.json \
 --prompt "A cinematic fox walking through a snowy forest" \
---save_result_path ${lightx2v_path}/save_results/output_lightx2v_minimax_h3_t2av_tp.mp4 \
+--save_result_path ${lightx2v_path}/save_results/output_lightx2v_minimax_h3_t2av_tp4.mp4 \
 --seed 42
